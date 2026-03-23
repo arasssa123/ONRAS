1fdd9c9976c406f3c4b92280d6c5b3d9e3a0bc9c
arasssa123 <onrassahin@gmail.com>
2026-03-23 19:28:52 +0300

    Change app.run to use environment variables for host and port

--- app.py
+++ app.py
@@ -104,5 +104,7 @@ def analiz():
     return render_template("analiz.html", toplam=toplam)
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
